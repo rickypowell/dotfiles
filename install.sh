bfed64db32a1bf7031adccb69474f6fa849e6289
Ricky Powell <rickypowell@users.noreply.github.com>
2024-01-09 16:24:44 -0600

    update install.sh for starship install

--- install.sh
+++ install.sh
@@ -1,10 +1,14 @@
 #!/bin/zsh
 
+# Alacritty configuration
 ln -s alacritty/.config/alacritty ~/.config/alacritty
-ln -s starship/.config/starship.toml ~/.config/starship.toml 
 
 # Alacritty themes
 # We use Alacritty's default Linux config directory as our storage location here.
 mkdir -p ~/.config/alacritty/themes
 git clone https://github.com/alacritty/alacritty-theme ~/.config/alacritty/themes
+
+# Starship command line prompt
+ln -s starship/.config/starship.toml ~/.config/starship.toml 
+brew install starship
 

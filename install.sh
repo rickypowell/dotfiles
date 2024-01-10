3e9db2fb717cf06995e52fe3aad0ef3e3ed95e9b
Ricky Powell <rickypowell@users.noreply.github.com>
2024-01-10 16:15:51 -0600

    added nvim with symlink updates

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 # Alacritty configuration
-ln -s alacritty/.config/alacritty ~/.config/alacritty
+ln -s "$PWD/alacritty/.config/alacritty" ~/.config/alacritty
 
 # Alacritty themes
 # We use Alacritty's default Linux config directory as our storage location here.
@@ -9,6 +9,13 @@ mkdir -p ~/.config/alacritty/themes
 git clone https://github.com/alacritty/alacritty-theme ~/.config/alacritty/themes
 
 # Starship command line prompt
-ln -s starship/.config/starship.toml ~/.config/starship.toml 
+ln -s "$PWD/starship/.config/starship.toml" ~/.config/starship.toml 
 brew install starship
 
+# Nvim install
+brew install nvim
+
+# Nvim configuration 
+# Install ripgrep for grep search in Telescope
+brew install ripgrep
+ln -s "$PWD/nvim/.config/nvim" ~/.config/nvim

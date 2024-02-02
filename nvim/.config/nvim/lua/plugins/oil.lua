0729c179c1fa21f9249fde3b0725c67ac74476d3
Ricky Powell <rickypowell@users.noreply.github.com>
2024-02-02 11:21:48 -0600

    added Oil keymap

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -1,13 +1,17 @@
 return {
   'stevearc/oil.nvim',
 
-  opts = {
-     view_options = {
-      -- Show files and directories that start with "."
-      show_hidden = true,
-    }
-  },
-
   -- Optional dependencies
   dependencies = { "nvim-tree/nvim-web-devicons" },
+
+  config = function()
+    require("oil").setup({
+       view_options = {
+        -- Show files and directories that start with "."
+        show_hidden = true,
+      }
+    })
+
+    vim.keymap.set("n", "<leader>e", "<cmd>Oil<cr>", { desc = "Open file explorer" })
+  end
 }

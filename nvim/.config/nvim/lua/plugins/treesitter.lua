b985c5fb94566f2d9752ec4ca206a2107928fb70
Ricky Powell <rickypowell@users.noreply.github.com>
2024-05-31 11:18:36 -0500

    updated plugins added textobjects

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,12 +1,12 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
+  {
+    "nvim-treesitter/nvim-treesitter",
 
-  build = ":TSUpdate",
+    build = ":TSUpdate",
 
-  config = function()
-    local configs = require("nvim-treesitter.configs")
+    event = "VeryLazy",
 
-    configs.setup({
+    opts = {
       ensure_installed = {
         "c",
         "lua",
@@ -23,6 +23,39 @@ return {
       sync_install = false,
       highlight = { enable = true },
       indent = { enable = true },
-    })
-  end
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+
+    event = "VeryLazy",
+
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+    },
+
+    config = function()
+      require("nvim-treesitter.configs").setup({})
+      -- When in diff mode, we want to use the default
+      -- vim text objects c & C instead of the treesitter ones.
+      local move = require("nvim-treesitter.textobjects.move") ---@type table<string,fun(...)>
+      local configs = require("nvim-treesitter.configs")
+      for name, fn in pairs(move) do
+        if name:find("goto") == 1 then
+          move[name] = function(q, ...)
+            if vim.wo.diff then
+              local config = configs.get_module("textobjects.move")[name] ---@type table<string,string>
+              for key, query in pairs(config or {}) do
+                if q == query and key:find("[%]%[][cC]") then
+                  vim.cmd("normal! " .. key)
+                  return
+                end
+              end
+            end
+            return fn(q, ...)
+          end
+        end
+      end
+    end,
+  },
 }

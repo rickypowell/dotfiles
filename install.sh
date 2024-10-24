132763e5af1b12c86a7c1eb0772cb9bdbc37e206
Ricky <rickypowell@users.noreply.github.com>
2024-10-24 14:21:50 -0500

    optimize theme download

--- install.sh
+++ install.sh
@@ -26,6 +26,10 @@ stow --restow zellij
 
 # Alacritty themes
 # We use Alacritty's default Linux config directory as our storage location here.
-mkdir -p alacritty/.config/alacritty/themes
-git clone https://github.com/alacritty/alacritty-theme alacritty/.config/alacritty/themes
+if [ ! -d "alacritty/.config/alacritty/themes" ]; then
+    mkdir -p alacritty/.config/alacritty/themes
+    git clone https://github.com/alacritty/alacritty-theme alacritty/.config/alacritty/themes
+else
+    echo "Theme already exists"
+fi
 

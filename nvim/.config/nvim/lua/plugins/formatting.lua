44ad06909c8fcd29ed0f2111a26c4e0fc3b6e30f
Ricky Powell <rickypowell@users.noreply.github.com>
2025-09-30 10:31:19 -0500

    feat: prettier react and typesript

---
+++ nvim/.config/nvim/lua/plugins/formatting.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      local fmt_config = {
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 550,
+      }
+
+      local conform = require("conform")
+
+      conform.setup({
+        formatters_by_ft = {
+          typescript = { "prettier" },
+          typescriptreact = { "prettier" },
+        },
+        format_on_save = fmt_config,
+      })
+
+      vim.keymap({ "n", "v" }, "<leader>mp", function()
+        conform.format(fmt_config)
+      end, { desc = "Format file or range (in visual mode)" })
+    end,
+  },
+}

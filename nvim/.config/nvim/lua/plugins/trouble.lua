e1e465732cd5e612da3bb0308098e7a181c0e2a3
Ricky Powell <rickypowell@users.noreply.github.com>
2026-03-10 12:45:55 -0500

    feat(nvim): zig config

--- nvim/.config/nvim/lua/plugins/trouble.lua
+++ nvim/.config/nvim/lua/plugins/trouble.lua
@@ -14,7 +14,7 @@ return {
     vim.keymap.set("n", "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", { desc = "Toggle Trouble" })
     vim.keymap.set("n", "<leader>xw", "<cmd><cr>", { desc = "Toggle workspace Trouble" })
     vim.keymap.set("n", "<leader>xd", "<cmd><cr>", { desc = "Toggle document Trouble" })
-    vim.keymap.set("n", "<leader>xq", "<cmd><cr>", { desc = "Toggle quickfix Trouble" })
+    vim.keymap.set("n", "<leader>xq", "<cmd>Trouble qflist toggle<cr>", { desc = "Toggle quickfix Trouble" })
     vim.keymap.set("n", "<leader>xl", "<cmd><cr>", { desc = "Toggle loclist Trouble" })
     vim.keymap.set("n", "gR", "<cmd><cr>", { desc = "Toggle lsp refs Trouble" })
   end
